0ca3367cb93e89d613bf0ad599261e43e13bb8b4
David Tollmyr <david@burtcorp.com>
2012-01-17 17:58:17 +0100

    Initial commit

---
+++ lib/adtech.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+require 'java'
+$CLASSPATH << "lib/ext/HeliosWSClientSystem_1.10.1"
+$CLASSPATH << "lib/ext/HeliosWSClientSystem_1.10.1/lib"
+require 'ext/HeliosWSClientSystem_1.10.1/HeliosWSClientSystem'

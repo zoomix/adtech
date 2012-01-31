e272753f5db1c83742d4e77ac6e5c6460cabf11c
David Tollmyr <david@burtcorp.com>
2012-01-31 10:31:20 +0100

    Getting campaigns

--- lib/adtech/runner.rb
+++ lib/adtech/runner.rb
@@ -8,11 +8,17 @@ java_package 'de.adtech.helios'
 java_import 'java.security.NoSuchProviderException'
 java_import 'org.systinet.wasp.webservice.LookupException'
 java_import 'de.adtech.webservices.helios.client.HeliosWSClientSystem'
+java_import 'de.adtech.helios.AttributeOperatorValueExpression'
+java_import 'de.adtech.webservices.helios.lowLevel.constants.IAttributeOperatorValueExpression'
+java_import 'de.adtech.webservices.helios.lowLevel.constants.ICampaign'
+java_import 'de.adtech.helios.BoolExpression'
 
 module Adtech
   class Runner
     SERVER_URL = "https://ws.adtech.de".freeze
     WSDL_URL = "http://ws.adtech.de".freeze
+
+    attr_reader :helios
 
     def initialize(path_to_config_file, username, password)
       @config_destination_dir = "/tmp/adtech"
@@ -27,6 +33,21 @@ module Adtech
     def startup
       @helios = HeliosWSClientSystem.new
       @helios.init_services(SERVER_URL, WSDL_URL, @config_destination_dir, @username, @password)
+    end
+    
+    def get_campaigns
+      aove = AttributeOperatorValueExpression.new
+      aove.setAttribute("statusTypeId")
+      aove.setOperator(IAttributeOperatorValueExpression.OP_EQUAL)
+      aove.setValue(ICampaign.STATUS_ACTIVE)
+      
+      filter = java.util.Vector.new
+      filter.add(aove)
+
+      boolExpression = BoolExpression.new
+      boolExpression.setExpressions(aove)
+      
+      list = cs.getCampaignList(nil, nil, boolExpression, nil)
     end
   end
 end

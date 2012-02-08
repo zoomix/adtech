5048362e9d3c8fa8a3e98f1aea694495d25d81a2
David Tollmyr <david@burtcorp.com>
2012-02-08 10:04:16 +0100

    Added start and end dates

--- lib/adtech/campaign.rb
+++ lib/adtech/campaign.rb
@@ -2,7 +2,7 @@ module Adtech
   class Campaign
     FIELDS = [:name, :advertiser, :customer, :campaign_id, :cpm, :cpc, :flatfee, :target]
     attr_reader *FIELDS
-    attr_reader :banners
+    attr_reader :banners, :start_date, :end_date
     
     def initialize(init, advertiser = nil, customer = nil)
       if init.is_a?(Hash)
@@ -17,6 +17,9 @@ module Adtech
       FIELDS.each do |v|
         ret[v.to_s] = instance_variable_get("@#{v}")
       end
+      [:start_date, :end_date].each do |v|
+        ret[v.to_s] = instance_variable_get("@#{v}").to_i
+      end
       ret['banners'] = @banners.map {|b| b.to_h }
       ret
     end
@@ -29,6 +32,8 @@ module Adtech
       @cpc = api_campaign.pricingConfig.cpc
       @flatfee = api_campaign.pricingConfig.flatfee
       @target = api_campaign.pricingConfig.target
+      @start_date = Time.at(api_campaign.absoluteStartDate.getTime/1000)
+      @end_date = Time.at(api_campaign.absoluteEndDate.getTime/1000)
       @advertiser = advertiser.name if advertiser
       @customer = customer.name if customer
       @banners = []
@@ -42,6 +47,9 @@ module Adtech
     def from_hash(hash)
       FIELDS.each do |v|
         instance_variable_set("@#{v}", hash[v.to_s])
+      end
+      [:start_date, :end_date].each do |v|
+        instance_variable_set("@#{v}", Time.at(hash[v.to_s]))
       end
       @banners = []
       hash['banners'].each do |b|
